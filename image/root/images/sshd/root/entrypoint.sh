c4487afed8ac4333a790de46cea49d691a8e4b8c
Emory Merryman <emory.merryman@gmail.com>
2017-08-18 15:58:39 +0000

    run sshd

--- image/root/images/sshd/root/entrypoint.sh
+++ image/root/images/sshd/root/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "${ID_RSA_PUB}" > ~/.ssh/authorized_keys &&
-    chmod 0600 ~/.ssh/authorized_keys
+    chmod 0600 ~/.ssh/authorized_keys &&
+    /usr/sbin/sshd -D -f /etc/ssh/sshd_config
