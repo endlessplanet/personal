04e5448db9f3060f996b17d99f5ec1888ea89d6f
Emory Merryman <emory.merryman@gmail.com>
2017-08-18 02:37:47 +0000

    add post-commit script and configure

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker run --interactive --tty --volume /var/run/docker.sock:/var/run/docker.sock:ro --env DISPLAY --env ID_RSA="$(cat ~/.ssh/vxD3wgSe_id_rsa)" --env KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" personal
+docker \
+    run \
+    --interactive \
+    --tty \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --env DISPLAY \
+    --env ID_RSA="$(cat ~/.ssh/vxD3wgSe_id_rsa)" \
+    --env KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" \
+    --env USERNAME="Emory Merryman" \
+    --env EMAIL="emory.merryman@gmail.com" \
+    personal
--- image/root/images/cloud9/root/entrypoint.sh
+++ image/root/images/cloud9/root/entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 mkdir ~/.ssh &&
-chmod 0700 ~/.ssh &&
-echo "${ID_RSA}" > ~/.ssh/id_rsa &&
-chmod 0600 ~/.ssh/id_rsa &&
-echo "${KNOWN_HOSTS}" > ~/.ssh/known_hosts &&
-chmod 0600 ~/.ssh/known_hosts &&
-mkdir /workspace &&
-mkdir /workspace/${GROUP_NAME} &&
-mkdir /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
-cd /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
-git init &&
-git remote add origin git@github.com:${GROUP_NAME}/${PROJECT_NAME}.git &&
-git fetch origin ${BRANCH} &&
-git checkout origin/${BRANCH} &&
-forever /root/.c9/server.js -w /workspace/${PROJECT_NAME} --auth "user:password" --listen 0.0.0.0
+    chmod 0700 ~/.ssh &&
+    echo "${ID_RSA}" > ~/.ssh/id_rsa &&
+    chmod 0600 ~/.ssh/id_rsa &&
+    echo "${KNOWN_HOSTS}" > ~/.ssh/known_hosts &&
+    chmod 0600 ~/.ssh/known_hosts &&
+    mkdir /workspace &&
+    mkdir /workspace/${GROUP_NAME} &&
+    mkdir /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
+    cd /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
+    git init &&
+    git config user.name "${USERNAME}" &&
+    git config user.email "${EMAIL}" &&
+    cp /opt/docker/post-commit.sh .git/hooks/post-commit &&
+    chmod 0500 .git/hooks/post-commit &&
+    git remote add origin git@github.com:${GROUP_NAME}/${PROJECT_NAME}.git &&
+    git fetch origin ${BRANCH} &&
+    git checkout origin/${BRANCH} &&
+    forever /root/.c9/server.js -w /workspace/${PROJECT_NAME} --auth "user:password" --listen 0.0.0.0
