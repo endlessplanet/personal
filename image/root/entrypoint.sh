f97a281c12a503b8a9d088a6596c59d8269d6172
Emory Merryman <emory.merryman@gmail.com>
2017-08-18 15:27:14 +0000

    reorg script

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+docker-compose build &&
 docker-compose stop &&
 docker-compose rm -fv &&
-docker-compose build &&
-docker-compose up -d
+docker-compose up -d &&
+docker-compose logs -f pass
