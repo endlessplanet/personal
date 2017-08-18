50f7e3b5af5225c5567d7edf3e0435cca26a5575
Emory Merryman <emory.merryman@gmail.com>
2017-08-18 11:05:08 +0000

    add pass

--- bin/run.sh
+++ bin/run.sh
@@ -10,4 +10,7 @@ docker \
     --env KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" \
     --env USERNAME="Emory Merryman" \
     --env EMAIL="emory.merryman@gmail.com" \
+    --env GPG_KEY_ID="D65D3F8C" \
+    --env GPG_PRIVATE_KEY="$(cat ~/important/gpg/secret1.key)" \
+    --env GPG_OWNER_TRUST="$(cat ~/important/gpg/owner1.trust)" \
     personal
--- image/root/images/pass/root/entrypoint.sh
+++ image/root/images/pass/root/entrypoint.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-mkdir ~/.ssh &&
+echo "${GPG_PRIVATE_KEY}" > /tmp/secret1.key &&
+    echo "${GPG_OWNER_TRUST}" > /tmp/owner1.trust &&
+    gpg --batch --import /tmp/secret1.key &&
+    gpg --batch --import-ownertrust /tmp/owner1.trust &&
+    pass init ${GPG_KEY_ID} &&
+    pass git init &&
+    pass git config user.name "${USER_NAME}" &&
+    pass git config user.email "${EMAIL}" &&
+    pass git remote add origin git@github.com:desertedscorpion/passwordstore.git
+    mkdir ~/.ssh &&
     chmod 0700 ~/.ssh &&
     echo "${ID_RSA}" > ~/.ssh/id_rsa &&
     chmod 0600 ~/.ssh/id_rsa &&
     echo "${KNOWN_HOSTS}" > ~/.ssh/known_hosts &&
     chmod 0600 ~/.ssh/known_hosts &&
     mkdir /workspace &&
-    mkdir /workspace/${GROUP_NAME} &&
-    mkdir /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
-    pass 
-    
-    cd /workspace/${GROUP_NAME}/${PROJECT_NAME} &&
-    git init &&
-    git config user.name "${USERNAME}" &&
-    git config user.email "${EMAIL}" &&
-    cp /opt/docker/post-commit.sh .git/hooks/post-commit &&
-    chmod 0500 .git/hooks/post-commit &&
-    git remote add origin git@github.com:${GROUP_NAME}/${PROJECT_NAME}.git &&
-    git fetch origin ${BRANCH} &&
-    git checkout origin/${BRANCH} &&
-    forever /root/.c9/server.js -w /workspace/${GROUP_NAME}/${PROJECT_NAME} --auth "user:password" --listen 0.0.0.0
+    mkdir /workspace/pass &&
+    forever /root/.c9/server.js -w /workspace/pass --auth "user:password" --listen 0.0.0.0
