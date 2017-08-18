4ee8f20a67ceeab0e2e881974408325422d1f1ae
Emory Merryman <emory.merryman@gmail.com>
2017-08-18 12:03:09 +0000

    add post-commit auto push to it

--- image/root/images/pass/root/entrypoint.sh
+++ image/root/images/pass/root/entrypoint.sh
@@ -8,13 +8,17 @@ echo "${GPG_PRIVATE_KEY}" > /tmp/secret1.key &&
     pass git init &&
     pass git config user.name "${USER_NAME}" &&
     pass git config user.email "${EMAIL}" &&
-    pass git remote add origin git@github.com:desertedscorpion/passwordstore.git
+    pass git remote add origin git@github.com:desertedscorpion/passwordstore.git &&
     mkdir ~/.ssh &&
     chmod 0700 ~/.ssh &&
     echo "${ID_RSA}" > ~/.ssh/id_rsa &&
     chmod 0600 ~/.ssh/id_rsa &&
     echo "${KNOWN_HOSTS}" > ~/.ssh/known_hosts &&
     chmod 0600 ~/.ssh/known_hosts &&
+    pass git fetch origin master &&
+    pass git rebase origin/master &&
+    cp /opt/docker/post-commit.sh ~/.password-store/.git/hooks/post-commit &&
+    chmod 0555 ~/.password-store/.git/hooks/post-commit &&
     mkdir /workspace &&
     mkdir /workspace/pass &&
     forever /root/.c9/server.js -w /workspace/pass --auth "user:password" --listen 0.0.0.0
