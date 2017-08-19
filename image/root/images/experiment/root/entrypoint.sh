abb380755d18c01db983c6244fec568067c57288
Emory Merryman <emory.merryman@gmail.com>
2017-08-19 12:35:05 +0000

    use named workspace

--- image/root/images/experiment/root/entrypoint.sh
+++ image/root/images/experiment/root/entrypoint.sh
@@ -2,7 +2,15 @@
 
 if [ ! -f ~/.ssh/id_rsa ]
 then
-        echo "${ID_RSA}" > ~/.ssh/id_rsa &&
-        chmod 0600 ~/.ssh/id_rsa
+    echo "${ID_RSA}" > ~/.ssh/id_rsa &&
+    chmod 0600 ~/.ssh/id_rsa
 fi &&
-    forever ~/.c9/server.js -w /workspace/pass --auth "user:password" --listen 0.0.0.0
+    if [ ! -d ~/workspace ]
+    then
+        mkdir /workspace
+    fi &&
+    if [ ! -d ~/workspace/${PROJECT_NAME} ]
+    then
+        mkdir /workspace/${PROJECT_NAME}
+    fi &&
+    forever ~/.c9/server.js -w /workspace/${PROJECT_NAME} --auth "user:password" --listen 0.0.0.0
