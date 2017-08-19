f903fcdfe92a66bda23b2ffd781877fe3c12fb24
Emory Merryman <emory.merryman@gmail.com>
2017-08-19 22:16:47 +0000

    fix typo

--- image/root/images/sshd/root/run.sh
+++ image/root/images/sshd/root/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 sed -i "s,#GatewayPorts no,GatewayPorts yes," /etc/ssh/sshd_config &&
-    echo ALPHA &&
-    cat /opt/docker/authorized_keys /root/.ssh/authorized_keys &&
+    cat /opt/docker/authorized_keys > /root/.ssh/authorized_keys &&
     chmod 0600 /root/.ssh/authorized_keys
