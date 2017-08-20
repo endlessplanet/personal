d1204ed265fc7528a04fcefbfb956c3ce7138d13
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 10:55:10 +0000

    use local host

--- image/root/images/experiment/root/entrypoint.sh
+++ image/root/images/experiment/root/entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -d ~/workspace/${PROJECT_NAME} ]
 then
     mkdir /workspace/${PROJECT_NAME}
 fi &&
-    forever ~/.c9/server.js -w /workspace/${PROJECT_NAME} --auth "user:password" --listen 0.0.0.0
+    forever ~/.c9/server.js -w /workspace/${PROJECT_NAME} --auth "user:password" --listen 127.0.0.1
