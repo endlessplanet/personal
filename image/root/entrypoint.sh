a2ee12d2639f2b61204cb7116ed847583b160d52
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 14:27:35 +0000

    try to not require auth on personal

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -3,13 +3,18 @@
 docker-compose build &&
     docker-compose stop &&
     docker-compose rm -fv &&
-    docker-compose up -d chromium sshd pass experiment &&
+    docker-compose up -d chromium sshd personal pass experiment &&
     
     sleep 10s &&
     docker-compose ps &&
     docker-compose logs pass &&
     
-    echo PASS 100 &&
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root personal ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root personal ssh -fN sshd2  </dev/null >/tmp/sshd2.log 2>&1 &) &&
+    sleep 10s
+    
     sleep 10s &&
     (nohup docker-compose exec -T --user root pass ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
     sleep 10s &&
--- image/root/images/cloud9/root/run.sh
+++ image/root/images/cloud9/root/run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 apk update &&
-apk upgrade &&
-apk add --no-cache git &&
-apk add --no-cache openssh &&
-apk add --no-cache docker &&
-rm -rf /var/cache/apk/*
+    apk upgrade &&
+    apk add --no-cache git &&
+    apk add --no-cache openssh &&
+    apk add --no-cache docker &&
+    mkdir /root/.ssh &&
+    chmod 0700 /root/.ssh &&
+    cp /opt/docker/id_rsa /root/.ssh/id_rsa &&
+    chmod 0600 /root/.ssh/id_rsa &&
+    cp /opt/docker/config /root/.ssh/config &&
+    chmod 0600 /root/.ssh/config &&
+    rm -rf /var/cache/apk/*
