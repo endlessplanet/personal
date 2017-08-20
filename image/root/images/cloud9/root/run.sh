b586e9a15f97c842f35f0f099bd1c623fcdd91e6
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 14:45:55 +0000

    add user user to personal

--- image/root/images/cloud9/root/run.sh
+++ image/root/images/cloud9/root/run.sh
@@ -5,6 +5,11 @@ apk update &&
     apk add --no-cache git &&
     apk add --no-cache openssh &&
     apk add --no-cache docker &&
+    adduser -D user &&
+    mkdir /workspace &&
+    chown user:user /workspace &&
+    cp -r /root/.c9 /home/user/.c9 &&
+    chown --recursive user:user /home/user/.c9 &&
     mkdir /root/.ssh &&
     chmod 0700 /root/.ssh &&
     cp /opt/docker/id_rsa /root/.ssh/id_rsa &&
