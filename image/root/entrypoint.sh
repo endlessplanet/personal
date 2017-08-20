bbe8580676729623091f1804c431e2bff195d2de
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 10:00:10 +0000

    sleep 10 seconds before trying it

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -4,6 +4,7 @@ docker-compose build &&
 docker-compose stop &&
 docker-compose rm -fv &&
 docker-compose up -d chromium sshd experiment &&
+sleep 10s &&
 docker-compose exec --user root experiment ssh -fN sshd1 &&
 sleep 10s &&
 docker-compose exec --user root experiment ssh -fN sshd2
