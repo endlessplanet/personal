e9ce6abed41e41bd54c02e198cb1d174facb07ae
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 10:42:54 +0000

    do not nohup second one

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -7,4 +7,4 @@ docker-compose up -d chromium sshd experiment &&
 sleep 10s &&
 (nohup docker-compose exec -T --user root experiment ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
 sleep 10s &&
-(nohup docker-compose exec -T --user root experiment ssh -fN sshd2  </dev/null >/tmp/sshd2.log 2>&1 &)
+docker-compose exec -T --user root experiment ssh -fN sshd2
