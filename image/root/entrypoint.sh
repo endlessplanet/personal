d5e8bee587a31e34d6749af4396188677d7b157b
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 14:08:37 +0000

    do ssh connections

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -10,10 +10,15 @@ docker-compose build &&
     docker-compose logs pass &&
     
     echo PASS 100 &&
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root pass ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root pass ssh -fN sshd2  </dev/null >/tmp/sshd2.log 2>&1 &) &&
+    sleep 10s
     
     echo EXPERIMENT
-    # sleep 10s &&
-    # (nohup docker-compose exec -T --user root experiment ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
-    # sleep 10s &&
-    # (nohup docker-compose exec -T --user root experiment ssh -fN sshd2  </dev/null >/tmp/sshd2.log 2>&1 &) &&
-    # sleep 10s
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root experiment ssh -fN sshd1  </dev/null >/tmp/sshd1.log 2>&1 &) &&
+    sleep 10s &&
+    (nohup docker-compose exec -T --user root experiment ssh -fN sshd2  </dev/null >/tmp/sshd2.log 2>&1 &) &&
+    sleep 10s
