18fbb5c803f7e5e6218bf387abea521b6d5de3ba
Emory Merryman <emory.merryman@gmail.com>
2017-08-20 08:19:46 +0000

    bash it

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 docker-compose build &&
-docker-compose stop &&
-docker-compose rm -fv &&
-docker-compose up -d &&
-docker-compose exec --user root ssh sshd1 
-docker-compose exec experiment ssh -fN sshd1 &&
-sleep 10s &&
-docker-compose exec experiment ssh -fN sshd2
+bash
+# docker-compose stop &&
+# docker-compose rm -fv &&
+# docker-compose up -d &&
+# docker-compose exec --user root ssh sshd1 
+# docker-compose exec experiment ssh -fN sshd1 &&
+# sleep 10s &&
+# docker-compose exec experiment ssh -fN sshd2
