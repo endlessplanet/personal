b3438e33939bd4eda4e0d23a3d7164334290562f
Emory Merryman <emory.merryman@gmail.com>
2017-09-10 02:06:17 +0000

    install ant jdk

--- image/root/run.sh
+++ image/root/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dnf update --assumeyes &&
-    dnf install --assumeyes 
+    dnf install --assumeyes java-1.8.0-openjdk-devel ant &&
     adduser user &&
     dnf update --assumeyes &&
     dnf clean all
