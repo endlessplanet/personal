c2b06d7591b2eab5073f9e02f19ed2123ab9d565
Emory Merryman <emory.merryman@gmail.com>
2017-10-03 13:16:45 +0000

    renamed

--- bin/exec.sh
+++ bin/exec.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 cleanup(){
-    docker container stop $(cat dind0.id) $(cat dind1.id) $(cat personal.id) &&
-        docker rm --volumes $(cat dind0.id) $(cat dind1.id) $(cat personal.id) &&
+    docker container stop $(cat manager.id) $(cat worker-01.id) $(cat personal.id) &&
+        docker rm --volumes $(cat manager.id) $(cat worker-01.id) $(cat personal.id) &&
         docker network rm $(cat network.id) &&
-        rm --force dind0.id dind1.id personal.id network.id
+        rm --force manager.id worker-01.id personal.id network.id
 }
     trap cleanup EXIT &&
     docker network create $(uuidgen) > network.id &&
     docker \
         container \
         create \
-        --cidfile dind0.id \
+        --cidfile manager.id \
         --privileged \
         --volume /tmp/.X11-unix:/var/opt/.X11-unix:ro \
         docker:17.09.0-ce-dind \
@@ -19,7 +19,7 @@ cleanup(){
     docker \
         container \
         create \
-        --cidfile dind1.id \
+        --cidfile worker-01.id \
         --privileged \
         --volume /tmp/.X11-unix:/var/opt/.X11-unix:ro \
         docker:17.09.0-ce-dind \
@@ -31,14 +31,14 @@ cleanup(){
         --interactive \
         --tty \
         --env DISPLAY \
-        --env DOCKER_HOST=tcp://docker1:2376 \
+        --env DOCKER_HOST=tcp://docker0:2376 \
         --volume /var/run/docker.sock:/var/run/docker.sock:ro \
         endlessplanet/personal:$(git rev-parse --verify HEAD) &&
-    docker network connect --alias docker0 $(cat network.id) $(cat dind0.id) &&
-    docker network connect --alias docker1 $(cat network.id) $(cat dind1.id) &&
+    docker network connect --alias docker0 $(cat network.id) $(cat manager.id) &&
+    docker network connect --alias docker1 $(cat network.id) $(cat worker-01.id) &&
     docker network connect $(cat network.id) $(cat personal.id)
-    docker container start $(cat dind0.id) $(cat dind1.id) &&
-    docker container exec --interactive --tty $(cat dind0.id) docker swarm init --advertise-addr docker0 &&
-    JOIN_TOKEN=$(docker container exec --interactive --tty $(cat dind0.id) docker swarm join-token --quiet worker | tr -cd "[:print:]") &&
-    docker container exec --interactive --tty $(cat dind1.id) docker swarm join --token "${JOIN_TOKEN}" docker0:2377 &&
+    docker container start $(cat manager.id) $(cat worker-01.id) &&
+    docker container exec --interactive --tty $(cat manager.id) docker swarm init --advertise-addr docker0 &&
+    JOIN_TOKEN=$(docker container exec --interactive --tty $(cat manager.id) docker swarm join-token --quiet worker | tr -cd "[:print:]") &&
+    docker container exec --interactive --tty $(cat worker-01.id) docker swarm join --token "${JOIN_TOKEN}" docker0:2377 &&
     docker container start --interactive $(cat personal.id)
