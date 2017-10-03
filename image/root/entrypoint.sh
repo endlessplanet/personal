b14a2432e95406a40cc933e381aba8a8a31edbc4
Emory Merryman <emory.merryman@gmail.com>
2017-10-03 10:34:32 +0000

    pull and create a network

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-# docker volume create home &&
+docker image pull sassmann/debian-chromium:latest &&
+    docker image pull gitlab/gitlab-ce:10.0.2-ce.0 &&
+    docker image pull gitlab/gitlab-runner:v10.0.1 &&
+    docker network create --driver overlay swarm-network &&
     sh
