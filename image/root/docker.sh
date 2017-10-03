c9e4755fd898b1ace217111b0446fbd37f11ffa1
Emory Merryman <emory.merryman@gmail.com>
2017-10-03 01:17:12 +0000

    dumb it down

--- image/root/docker.sh
+++ image/root/docker.sh
@@ -2,10 +2,10 @@
 
 CIDFILE=$(mktemp) &&
     cleanup(){
-        /usr/bin/docker container stop $(cat ${CIDFILE}) &&
-            docker container rm --volumes $(cat ${CIDFILE}) &&
+        /usr/bin/docker stop $(cat ${CIDFILE}) &&
+            docker rm --volumes $(cat ${CIDFILE}) &&
             rm ${CIDFILE}
     } &&
     rm ${CIDFILE} &&
-    /usr/bin/docker container create --cid ${CIDFILE} --interactive --tty docker:17.09.0-ce "${@}" &&
-    /usr/bin/docker container start --interactive $(cat ${CIDFILE})
+    /usr/bin/docker create --cid ${CIDFILE} --interactive --tty docker:17.09.0-ce "${@}" &&
+    /usr/bin/docker start --interactive $(cat ${CIDFILE})
