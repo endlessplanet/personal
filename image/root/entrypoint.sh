e5543720a9d01d87b9e2582b221210c4b6ea5cba
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 11:34:41 +0000

    use commands not stack

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-docker stack deploy --compose-file /opt/docker/docker-compose.yml xxx &&
+cleanup() {
+    true
+} &&
+    docker \
+        service \
+        create \
+        --name gitlab \
+        gitlab/gitlab-ce:latest &&
+    docker \
+        service \
+        create \
+        --name chromium \
+        --env DISPLAY \
+        --mount type=volume,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+        sassmann/debian-chromium:latest &&
     sh
