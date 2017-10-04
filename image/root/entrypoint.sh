ff6b033591c7df5ed0f678d7c7fcad17b6403b1a
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 11:41:54 +0000

    fix detach

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -6,13 +6,13 @@ cleanup() {
     docker \
         service \
         create \
-        --detach true \
+        --detach=true \
         --name gitlab \
         gitlab/gitlab-ce:latest &&
     docker \
         service \
         create \
-        --detach true \
+        --detach=true \
         --env DISPLAY \
         --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         sassmann/debian-chromium:latest &&
