000545ee16c61cce5bfe51582916387b7593a320
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 11:51:16 +0000

    wtf

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -16,4 +16,9 @@ cleanup() {
         --env DISPLAY \
         --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         sassmann/debian-chromium:latest &&
+    echo docker service ps gitlab --format {{.DesiredState}} &&
+    # ls -1 ${HOME}/data/gitlab-data | while read FILE
+    # do
+        
+    # done &&
     sh
