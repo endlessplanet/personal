10569027a3527af06e65e292c0a25a53eab15662
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 11:40:36 +0000

    fix error in mount

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -6,13 +6,14 @@ cleanup() {
     docker \
         service \
         create \
+        --detach true \
         --name gitlab \
         gitlab/gitlab-ce:latest &&
     docker \
         service \
         create \
-        --name chromium \
+        --detach true \
         --env DISPLAY \
-        --mount type=volume,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+        --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         sassmann/debian-chromium:latest &&
     sh
