d209cebe7fbdf36bc598ce4f3b0f03a4a8b27160
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 03:02:31 +0000

    use stack

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
-docker image pull sassmann/debian-chromium:latest &&
-    docker image pull gitlab/gitlab-ce:10.0.2-ce.0 &&
-    # docker image pull gitlab/gitlab-runner:v10.0.1 &&
-    docker network create --driver overlay --subnet 10.0.3.0/24 swarm-network &&
-    docker volume create homey &&
-    docker volume create workspace &&
-    # --device /dev/dri/card0 -v /run/user/$UID/pulse/native:/tmp/pulse -v /dev/shm:/home/user/Download \
-    docker \
-        service \
-        create \
-        --env DISPLAY \
-        --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
-        --name chromium \
-        --hostname chromium \
-        docker.io/sassmann/debian-chromium:latest &&
-    docker \
-        service \
-        create \
-        --publish 443:443 \
-        --publish 80:80 \
-        --publish 22:22 \
-        --name gitlab \
-        --hostname gitlab \
-        gitlab/gitlab-ce:latest &&
-    docker service update gitlab --network-add swarm-network &&
-    docker service update chromium --network-add swarm-network &&
+# docker image pull sassmann/debian-chromium:latest &&
+#     docker image pull gitlab/gitlab-ce:10.0.2-ce.0 &&
+#     # docker image pull gitlab/gitlab-runner:v10.0.1 &&
+#     docker network create --driver overlay --subnet 10.0.3.0/24 swarm-network &&
+#     docker volume create homey &&
+#     docker volume create workspace &&
+#     # --device /dev/dri/card0 -v /run/user/$UID/pulse/native:/tmp/pulse -v /dev/shm:/home/user/Download \
+#     docker \
+#         service \
+#         create \
+#         --env DISPLAY \
+#         --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+#         --name chromium \
+#         --hostname chromium \
+#         docker.io/sassmann/debian-chromium:latest &&
+#     docker \
+#         service \
+#         create \
+#         --publish 443:443 \
+#         --publish 80:80 \
+#         --publish 22:22 \
+#         --name gitlab \
+#         --hostname gitlab \
+#         gitlab/gitlab-ce:latest &&
+#     docker service update gitlab --network-add swarm-network &&
+#     docker service update chromium --network-add swarm-network &&
     sh
--- image/root/run.sh
+++ image/root/run.sh
@@ -4,6 +4,8 @@ apk update &&
     apk upgrade &&
     apk add --no-cache docker &&
     adduser -D user &&
+    cp /opt/docker/docker-compose.yml /home/user &&
+    chown user:user /home/user/docker-compose.yml &&
     # ls -1 /opt/docker/bin | while read FILE
     # do
     #     cp /opt/docker/bin/${FILE} /usr/local/bin/${FILE%.*} &&
