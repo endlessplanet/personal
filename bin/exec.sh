3b78f280a25927f965799b8942714cea37279185
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 00:29:41 +0000

    try using data addr

--- bin/exec.sh
+++ bin/exec.sh
@@ -3,12 +3,12 @@
 cleanup(){
     docker container stop $(cat manager.id) $(cat worker-00.id) $(cat worker-01.id) $(cat personal.id) &&
         docker rm --volumes $(cat manager.id) $(cat worker-00.id) $(cat worker-01.id) $(cat personal.id) &&
-        docker network rm $(cat advertise.id) $(cat listen.id) &&
-        rm --force manager.id worker-00.id worker-01.id personal.id advertise.id listen.id
+        docker network rm $(cat advertise.id) $(cat data.id) &&
+        rm --force manager.id worker-00.id worker-01.id personal.id advertise.id data.id
 }
     trap cleanup EXIT &&
     docker network create $(uuidgen) --subnet 10.0.0.0/24 > advertise.id &&
-    docker network create $(uuidgen) --subnet 10.0.1.0/24 > listen.id &&
+    docker network create $(uuidgen) --subnet 10.0.1.0/24 > data.id &&
     docker \
         container \
         create \
@@ -44,15 +44,15 @@ cleanup(){
         --volume /var/run/docker.sock:/var/run/docker.sock:ro \
         endlessplanet/personal:$(git rev-parse --verify HEAD) &&
     docker network connect --alias manager --ip 10.0.0.200 $(cat advertise.id) $(cat manager.id) &&
-    docker network connect --alias manager --ip 10.0.1.200 $(cat listen.id) $(cat manager.id) &&
+    docker network connect --ip 10.0.1.200 $(cat data.id) $(cat manager.id) &&
     docker network connect $(cat advertise.id) $(cat worker-00.id) &&
-    docker network connect $(cat listen.id) $(cat worker-00.id) &&
+    docker network connect $(cat data.id) $(cat worker-00.id) &&
     docker network connect $(cat advertise.id) $(cat worker-01.id) &&
-    docker network connect $(cat listen.id) $(cat worker-01.id) &&
+    docker network connect $(cat data.id) $(cat worker-01.id) &&
     docker network connect $(cat advertise.id) $(cat personal.id) &&
     docker container start $(cat manager.id) $(cat worker-00.id) $(cat worker-01.id) &&
-    docker container exec --interactive --tty $(cat manager.id) docker swarm init --advertise-addr 10.0.0.200 --listen-addr 10.0.1.200 &&
-    JOIN_TOKEN=$(docker container exec --interactive --tty $(cat manager.id) docker swarm join-token --quiet worker | tr -cd "[:print:]") &&
+    docker container exec --interactive --tty $(cat manager.id) docker swarm init --advertise-addr 10.0.0.200 &&
+    JOIN_TOKEN=$(docker container exec --interactive --tty $(cat manager.id) docker swarm join-token --quiet --advertise--addr worker | tr -cd "[:print:]") &&
     docker container exec --interactive --tty $(cat worker-00.id) docker swarm join --token "${JOIN_TOKEN}" manager:2377 &&
     docker container exec --interactive --tty $(cat worker-01.id) docker swarm join --token "${JOIN_TOKEN}" manager:2377 &&
     docker container start --interactive $(cat personal.id)
