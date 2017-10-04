83f38574d7ddc91e2b80e1802815645fb448b7df
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 11:27:50 +0000

    cleanup

--- image/root/bin/fedora.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker container run --interactive --tty fedora:26 dnf provides "*bin/${@}" &&
-    
--- image/root/run.sh
+++ image/root/run.sh
@@ -4,11 +4,4 @@ apk update &&
     apk upgrade &&
     apk add --no-cache docker &&
     adduser -D user &&
-    cp /opt/docker/docker-compose.yml /home/user &&
-    chown user:user /home/user/docker-compose.yml &&
-    # ls -1 /opt/docker/bin | while read FILE
-    # do
-    #     cp /opt/docker/bin/${FILE} /usr/local/bin/${FILE%.*} &&
-    #         chmod 0555 /usr/local/bin/${FILE%.*}
-    # done &&
     rm -rf /var/cache/apk/*
