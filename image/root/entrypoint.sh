1a4a3b9d42f86853ec11968b4e3114efa226d507
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 12:35:34 +0000

    do not use detach

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -8,14 +8,12 @@ WORKDIR=$(mktemp -d) &&
     docker \
         service \
         create \
-        --detach=true \
         --hostname gitlab \
         --name gitlab \
         gitlab/gitlab-ce:latest &&
     docker \
         service \
         create \
-        --detach=true \
         --env DISPLAY \
         --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         sassmann/debian-chromium:latest &&
