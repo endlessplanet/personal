0431c6862675c0ad989b749839aff116900a088e
Emory Merryman <emory.merryman@gmail.com>
2017-10-04 12:23:59 +0000

    add quote health checks

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-cleanup() {
-    true
-} &&
+WORKDIR=$(mktemp -d) &&
+    cleanup() {
+        rm -rf ${WORKDIR} &&
+            true
+    } &&
     docker \
         service \
         create \
         --detach=true \
+        --host-name gitlab \
         --name gitlab \
         gitlab/gitlab-ce:latest &&
     docker \
@@ -16,9 +19,16 @@ cleanup() {
         --env DISPLAY \
         --mount type=bind,source=/var/opt/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         sassmann/debian-chromium:latest &&
-    echo docker service ps gitlab --format {{.DesiredState}} &&
+    while [[ ! "$(docker service ps gitlab --format {{.CurrentState}})" =~ "Running" ]]
+    do
+        echo waiting for gitlab to start &&
+            sleep 10s
+    done &&
+    echo gitlab has started &&
     # ls -1 ${HOME}/data/gitlab-data | while read FILE
     # do
-        
+    #     cp ${HOME}/data/gitlab-data/${FILE} ${WORKDIR}/${FILE} &&
+    #     cd ${WORKDIR} &&
+    #     rm -f ${FILE}
     # done &&
-    sh
+    bash
