436054a9d8d47a0d8597b92f076fcbacb86f0a0a
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 10:37:27 +0000

    use mount properly

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -3,7 +3,7 @@
 export PATH=${HOME}/bin:${PATH} &&
     (cd dockerfiles && sh build-images.sh) &&
     docker image pull gitlab/gitlab-ce:latest &&
-    docker container create --restart always --mount /srv/root/tmp/.X11-unix:/tmp/.X11-unix:ro firefox &&
+    docker container create --restart always --mount type=bind,source=/srv/root/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true firefox &&
     docker container ps --all --quiet | while read ID
     do
         docker container start ${ID}
