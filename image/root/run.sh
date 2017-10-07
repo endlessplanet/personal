0400923f9a508f2942f11a1f456edea93411c180
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 02:42:21 +0000

    write docker files

--- image/root/run.sh
+++ image/root/run.sh
@@ -4,11 +4,13 @@ apk update &&
     apk upgrade &&
     apk add --no-cache bash util-linux libxslt &&
     adduser -D user &&
-    mkdir /home/user/bin &&
+    mkdir /home/user/bin /home/user/dockerfiles &&
     ls -1 /opt/docker/bin | while read FILE
     do
         cp /opt/docker/bin/${FILE} /home/user/bin/${FILE%.*} &&
             chmod 0500 /home/user/bin/${FILE%.*}
     done &&
-    chown -R user:user /home/user/bin &&
+    xsltproc /opt/docker/style/alpine.xslt.xml /opt/data.xml > /tmp/alpine.xml &&
+    xsltproc /opt/docker/style/docker.xslt.xml /tmp/alpine.xml &&
+    chown -R user:user /home/user/bin /home/user/dockerfiles &&
     rm -rf /var/cache/apk/*
