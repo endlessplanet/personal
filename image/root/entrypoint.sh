bb3e444ec5c252fe60b48d3447b535bad8d86b28
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 11:40:02 +0000

    do not build images, start chromium and gitlab

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 export PATH=${HOME}/bin:${PATH} &&
-    (cd dockerfiles && sh build-images.sh) &&
     docker image pull gitlab/gitlab-ce:latest &&
+    docker image pull xueshanf/awscli &&
+    docker image pull sassmann/debian-chromium &&
     docker \
         container \
         create \
         --restart always \
         --mount type=bind,source=/srv/root/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
         --env DISPLAY \
-        firefox &&
+        sassmann/debian-chromium &&
     docker \
         container \
         create \
         --name gitlab \
-        --mount type=volume,source=gitlab-config,destination=/etc/gitlab,readonly=true \
+        --restart always \
         gitlab/gitlab-ce:latest &&
     docker container ps --all --quiet | while read ID
     do
