5d8591d4141a3fbada2104a065155feef58d1456
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 12:47:53 +0000

    try to configure gitlab

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,16 +1,44 @@
 #!/bin/sh
 
 export PATH=${HOME}/bin:${PATH} &&
-    docker image pull gitlab/gitlab-ce:latest &&
-    docker image pull xueshanf/awscli &&
-    docker image pull sassmann/debian-chromium &&
+    docker network create system &&
+    docker volume create gitlab-config &&
+    cat \
+        /opt/docker/openssl.txt | docker \
+        container \
+        run \
+        --interactive \
+        --rm \
+        --mount type=volume,source=gitlab-config,destination=/etc/gitlab \
+        --workdir /etc/gitlab \
+        jordi/openssl \
+            openssl \
+            req -x509 \
+            -newkey rsa:4096 \
+            -keyout gitlab.key \
+            -out gitlab.crt \
+            -days 365 \
+            -nodes &&
+    cat \
+        /opt/docker/gitlab.rb | docker \
+        container \
+        run \
+        --interactive \
+        --rm \
+        --mount type=volume,source=gitlab-config,destination=/etc/gitlab \
+        --workdir /etc/gitlab \
+        alpine:3.4 \
+            tee \
+            gitlab.rb &&
     docker \
         container \
-        create \
-        --name chromium \
+        run \
+        --detach \
         --restart always \
         --mount type=bind,source=/srv/root/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+        --mount type=volume,source=gitlab-config,destination=/etc/gitlab,readonly=true \
         --env DISPLAY \
+        --network system \
         sassmann/debian-chromium &&
     docker \
         container \
@@ -18,11 +46,6 @@ export PATH=${HOME}/bin:${PATH} &&
         --name gitlab \
         --restart always \
         gitlab/gitlab-ce:latest &&
-    docker network create system &&
-    docker network connect system chromium &&
     docker network connect --alias gitlab system gitlab &&
-    docker container ps --all --quiet | while read ID
-    do
-        docker container start ${ID}
-    done &&
+    docker container start gitlab &&
     bash
