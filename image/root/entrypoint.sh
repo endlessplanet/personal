81a75232053280fdf7220ddf5a474f8964350b29
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 10:55:19 +0000

    use env and add gitlab

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -3,7 +3,19 @@
 export PATH=${HOME}/bin:${PATH} &&
     (cd dockerfiles && sh build-images.sh) &&
     docker image pull gitlab/gitlab-ce:latest &&
-    docker container create --restart always --mount type=bind,source=/srv/root/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true firefox &&
+    docker \
+        container \
+        create \
+        --restart always \
+        --mount type=bind,source=/srv/root/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
+        --env DISPLAY \
+        firefox &&
+    docker \
+        container \
+        create \
+        --name gitlab \
+        --mount type=volume,source=gitlab-config,destination=/etc/gitlab,readonly=true \
+        gitlab/gitlab-ce:latest &&
     docker container ps --all --quiet | while read ID
     do
         docker container start ${ID}
