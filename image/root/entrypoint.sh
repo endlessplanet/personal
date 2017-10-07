1273d6d0a9932fb7e3564e0c0fe76e0a1ff5d9bd
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 10:30:08 +0000

    start firefox and be quiet

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -4,7 +4,7 @@ export PATH=${HOME}/bin:${PATH} &&
     (cd dockerfiles && sh build-images.sh) &&
     docker image pull gitlab/gitlab-ce:latest &&
     docker container create --restart always --mount /srv/root/tmp/.X11-unix:/tmp/.X11-unix:ro firefox &&
-    docker container ps --all | while read ID
+    docker container ps --all --quiet | while read ID
     do
         docker container start ${ID}
     done &&
