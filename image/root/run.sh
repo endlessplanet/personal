c704f33841e802f394de2968aa7c3bdec02df66e
Emory Merryman <emory.merryman@gmail.com>
2017-10-07 02:52:11 +0000

    write docker files

--- image/root/run.sh
+++ image/root/run.sh
@@ -10,7 +10,7 @@ apk update &&
         cp /opt/docker/bin/${FILE} /home/user/bin/${FILE%.*} &&
             chmod 0500 /home/user/bin/${FILE%.*}
     done &&
-    xsltproc /opt/docker/style/alpine.xslt.xml /opt/data.xml > /tmp/alpine.xml &&
+    xsltproc /opt/docker/style/alpine.xslt.xml /opt/docker/data.xml > /tmp/alpine.xml &&
     xsltproc /opt/docker/style/docker.xslt.xml /tmp/alpine.xml &&
     chown -R user:user /home/user/bin /home/user/dockerfiles &&
     rm -rf /var/cache/apk/*
