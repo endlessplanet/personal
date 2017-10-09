6e2214e5b6c1295d3a8b63fa0d24cbe1c0a0d34a
Emory Merryman <emory.merryman@gmail.com>
2017-10-09 22:32:00 +0000

    try to add gitlab-runner

--- image/root/bin/cleanup.sh
+++ image/root/bin/cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker exec --interactive --tty gitlab gitlab-rake gitlab:backup:create
+docker exec --interactive --tty gitlab-runner gitlab-runner unregister --name standard &&
+    docker exec --interactive --tty gitlab gitlab-rake gitlab:backup:create
