f122b8f664a793841c7b75a56f8e9b137c0c2c5d
Emory Merryman <emory.merryman@gmail.com>
2017-10-09 12:17:09 +0000

    when gitlab has finished starting, restart it and load backup

--- image/root/bin/refresh.sh
+++ image/root/bin/refresh.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-docker \
-    run \
-    --env AWS_ACCESS_KEY_ID \
-    --env AWS_SECRET_ACCESS_KEY \
-    --env AWS_DEFAULT_REGION \
-    --interactive \
-    --tty \
-    --rm \
-    --mount type=volume,source=gitlab-backup,destination=/var/backups \
-    --workdir /var/backups/gitlab \
-    xueshanf/awscli:latest \
-        aws \
-        s3 \
-        cp \
-        --include '*gitlab_backup.tar' \
-        --recursive \
-        s3://${GITLAB_BACKUP_BUCKET} /var/backups/gitlab/
-        
 docker container exec --interactive --tty gitlab gitlab-ctl reconfigure &&
     docker container exec --interactive --tty gitlab gitlab-ctl stop unicorn &&
     docker container exec --interactive --tty gitlab gitlab-ctl stop sidekiq &&
@@ -30,23 +12,23 @@ docker container exec --interactive --tty gitlab gitlab-ctl reconfigure &&
         --mount type=volume,source=gitlab-backup,destination=/var/backups \
         --workdir /var/backups/gitlab \
         alpine:3.4 \
-            ls -1 | sort | tail -n 1 | while read TSTAMP
+            ls -1 | sort | tail -n 1 | while read BACKUP2
             do
-                echo docker \
-                    container \
-                    exec \
-                    --interactive \
-                    gitlab \
-                    gitlab-rake \
-                    gitlab:backup:restore \
-                    BACKUP=${TSTAMP%.*} &&
-                docker \
-                    container \
-                    exec \
-                    --interactive \
-                    gitlab \
-                    gitlab-rake \
-                    gitlab:backup:restore \
-                    BACKUP=${TSTAMP%.*}
+                echo ${BACKUP2} &&
+                    echo ${BACKUP1} &&
+                    BACKUP1=${BACKUP2%_*} &&
+                    (cat <<EOF
+yes
+yes
+
+EOF
+                    ) | docker \
+                        container \
+                        exec \
+                        --interactive \
+                        gitlab \
+                        gitlab-rake \
+                        gitlab:backup:restore \
+                        BACKUP=${BACKUP1%_*}
             done &&
         docker container exec --interactive --tty gitlab gitlab-ctl start
--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -107,6 +107,43 @@ export PATH=${HOME}/bin:${PATH} &&
         --env DISPLAY \
         --network system \
         sassmann/debian-chromium &&
+    while [ $(docker inspect gitlab --format "{{ .State.Health.Status }}" gitlab) != "healthy" ]
+    do
+        docker inspect gitlab --format "{{ .State.Health.Status }}" gitlab &&
+            sleep 10s
+    done &&
+    docker container exec --interactive --tty gitlab gitlab-ctl reconfigure &&
+    docker container exec --interactive --tty gitlab gitlab-ctl stop unicorn &&
+    docker container exec --interactive --tty gitlab gitlab-ctl stop sidekiq &&
+    docker \
+        container \
+        run \
+        --interactive \
+        --rm \
+        --tty \
+        --mount type=volume,source=gitlab-backup,destination=/var/backups \
+        --workdir /var/backups/gitlab \
+        alpine:3.4 \
+            ls -1 | sort | tail -n 1 | while read BACKUP2
+            do
+                echo ${BACKUP2} &&
+                    echo ${BACKUP1} &&
+                    BACKUP1=${BACKUP2%_*} &&
+                    (cat <<EOF
+yes
+yes
+
+EOF
+                    ) | docker \
+                        container \
+                        exec \
+                        --interactive \
+                        gitlab \
+                        gitlab-rake \
+                        gitlab:backup:restore \
+                        BACKUP=${BACKUP1%_*}
+            done &&
+    docker container exec --interactive --tty gitlab gitlab-ctl start &&
     echo GITLAB_ROOT_PASSWORD=${GITLAB_ROOT_PASSWORD} &&
     echo GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN=${GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN} &&
     bash
