01cb3ada6467ea0b26a2e12f472542bafdaff223
Emory Merryman <emory.merryman@gmail.com>
2017-10-11 12:00:31 +0000

    this works to a point, I think the networks are not connected on the runner...

--- image/root/bin/cloud9.sh
+++ image/root/bin/cloud9.sh
@@ -10,4 +10,11 @@ CIDFILE=$(mktemp) &&
         sapk/cloud9 \
             --auth \
             user:password &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) mkdir /root/.ssh &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) chown 0700 /root/.ssh &&
+    echo "${ID_RSA}" | docker container exec --interactive $(cat ${CIDFILE}) tee /root/.ssh/id_rsa &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) chown 0600 /root/.ssh/id_rsa &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) apt-get update --assume-yes &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) apt-get install --assume-yes openssh-client git &&
+    docker container exec --interactive --tty $(cat ${CIDFILE}) apt-get clean &&
     docker network connect --alias ${1} system $(cat ${CIDFILE})
