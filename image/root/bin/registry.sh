5c59c7d75d1750d1ad3679a9592cdb73fa74913c
Emory Merryman <emory.merryman@gmail.com>
2017-10-13 23:50:50 +0000

    wtf

--- image/root/bin/registry.sh
+++ image/root/bin/registry.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-docker volume create registry-certs &&
+docker volume create auth &&
+    docker \
+        container \
+        run \
+        --entrypoint htpasswd \
+        --mount type=volume,source=auth,destination=/auth \
+        --workdir /auth \
+        registry:2.5.2 \
+            -cBb \
+            htpasswd \
+            user \
+            password &&
+    docker volume create registry-certs &&
     cat \
         /opt/docker/ssl/registry.txt | docker \
         container \
@@ -17,14 +29,21 @@ docker volume create registry-certs &&
             -out registry.crt \
             -days 365 \
             -nodes &&
-docker \
-    container \
-    run \
-    --publish-all \
-    --detach \
-    --mount type=volume,source=registry-certs,destination=/certs \
-    --env REGISTRY_HTTP_ADDR=0.0.0.0:80 \
-    --env REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
-    --env REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
-    --env REGISTRY_HTTP_SECRET=$(uuidgen) \
-    registry:2.5.2
+    docker \
+        container \
+        run \
+        --name registry \
+        --publish 443:443 \
+        --publish 5000:5000 \
+        --detach \
+        --mount type=volume,source=auth,destination=/auth \
+        --env REGISTRY_AUTH=htpasswd \
+        --env "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+        --env REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+        --mount type=volume,source=registry-certs,destination=/certs \
+        --env REGISTRY_HTTP_ADDR=0.0.0.0:80 \
+        --env REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
+        --env REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
+        --env REGISTRY_HTTP_SECRET=$(uuidgen) \
+        registry:2.5.2 &&
+    docker network connect --alias registry system registry
